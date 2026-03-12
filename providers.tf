86088799a8838521d1a00ba76b3d78f580464060
drumlast <drumlastic@gmail.com>
2026-03-12 14:19:07 +0300

    fix env

--- providers.tf
+++ providers.tf
@@ -10,7 +10,8 @@ terraform {
 }
 
 provider "yandex" {
+  token     = var.token
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
-  zone      = var.zone
-}
+  zone      = var.default_zone
+  }
